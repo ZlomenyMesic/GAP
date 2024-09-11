a217d733558217cbef0ad2629785870381211f18
ZlomenyMesic <michna.danecek@gmail.com>
2024-09-11 16:29:04 +0200

    Add project files.

---
+++ GAP/Program.cs
@@ -0,0 +1,15 @@
+﻿//
+// GAP - Generative Art Producer
+//   by ZlomenyMesic & KryKom
+//
+//      founded 11.9.2024
+//
+
+namespace GAP;
+
+class Program {
+    static int Main() {
+        Console.WriteLine("Hello, World!");
+        return 0;
+    }
+}
